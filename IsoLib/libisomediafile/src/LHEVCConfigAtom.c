ebe37ff923c49a4a32bb7d447d58e61be9a8c9b4
Dimitri Podborski <podborski@users.noreply.github.com>
2025-04-17 16:54:38 -0700

    header was copy&pasted form another file. Fix it

--- IsoLib/libisomediafile/src/LHEVCConfigAtom.c
+++ IsoLib/libisomediafile/src/LHEVCConfigAtom.c
@@ -1,9 +1,4 @@
-/**
- * @file HEVCConfigAtom.c
- * @brief Created for Nokia FAVS project by Tampere University of Technology
- * @version 0.1
- *
- * @copyright This software module was originally developed by Apple Computer, Inc. in the course of
+/* This software module was originally developed by Apple Computer, Inc. in the course of
  * development of MPEG-4. This software module is an implementation of a part of one or more MPEG-4
  * tools as specified by MPEG-4. ISO/IEC gives users of MPEG-4 free license to this software module
  * or modifications thereof for use in hardware or software products claiming conformance to MPEG-4.
@@ -15,7 +10,6 @@
  * own purpose, assign or donate the code to a third party and to inhibit third parties from using
  * the code for non MPEG-4 conforming products. This copyright notice must be included in all copies
  * or derivative works. Copyright (c) 1999.
- *
  */
 
 #include "MP4Atoms.h"
