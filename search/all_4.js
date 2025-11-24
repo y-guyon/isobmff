80f8760df2471848fa948e5d9811b3e174cf8dc8
podborski <podborski@users.noreply.github.com>
2025-11-24 20:00:15 +0000

    deploy: 1f9a174ff681f91d9183aafb91c1fb4f7cbfc9ad

--- search/all_4.js
+++ search/all_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['entitygroupentry_0',['entitygroupentry',['../struct_entity_group_entry.html',1,'EntityGroupEntry'],['../group___types.html#ga37ca966a1346fb5af27c0ebba8754f91',1,'EntityGroupEntry:&#160;ISOMovies.h']]]
+  ['entitygroupentry_0',['EntityGroupEntry',['../struct_entity_group_entry.html',1,'EntityGroupEntry'],['../group___types.html#ga37ca966a1346fb5af27c0ebba8754f91',1,'EntityGroupEntry:&#160;ISOMovies.h']]]
 ];
--- search/all_f.js
+++ search/all_f.js
@@ -5,7 +5,7 @@ var searchData=
   ['sample_5fgroup_5fauto_2',['SAMPLE_GROUP_AUTO',['../group___types.html#ggafd57078a76ff516ef5fc3ebf046e355ca968cda62b477325e8d8f0c364629e54b',1,'MP4Movies.h']]],
   ['sample_5fgroup_5fcompact_3',['SAMPLE_GROUP_COMPACT',['../group___types.html#ggafd57078a76ff516ef5fc3ebf046e355cab15834075f549d2846a9cc9b5bd0ab32',1,'MP4Movies.h']]],
   ['sample_5fgroup_5fnormal_4',['SAMPLE_GROUP_NORMAL',['../group___types.html#ggafd57078a76ff516ef5fc3ebf046e355cadfc060232e6fb240c4f11e10b098d9d0',1,'MP4Movies.h']]],
-  ['sampletogrouptype_5ft_5',['sampletogrouptype_t',['../group___types.html#gafd57078a76ff516ef5fc3ebf046e355c',1,'sampleToGroupType_t:&#160;MP4Movies.h'],['../group___types.html#gac01659b064e5f576d4390811e05de267',1,'sampleToGroupType_t:&#160;MP4Movies.h']]],
+  ['sampletogrouptype_5ft_5',['sampleToGroupType_t',['../group___types.html#gafd57078a76ff516ef5fc3ebf046e355c',1,'sampleToGroupType_t:&#160;MP4Movies.h'],['../group___types.html#gac01659b064e5f576d4390811e05de267',1,'sampleToGroupType_t:&#160;MP4Movies.h']]],
   ['slconfig_20functions_6',['SLConfig functions',['../group___s_l_config.html',1,'']]],
   ['support_7',['ISMACrypt Support',['../group___i_s_m_a.html',1,'']]],
   ['svcpps_8',['SVCpps',['../group___types.html#ggadc29c2ff13d900c2f185ee95427fb06ca050e90af13857641b15d3c8b7256bcbe',1,'ISOMovies.h']]],
